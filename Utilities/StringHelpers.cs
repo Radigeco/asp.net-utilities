15e651b33ed06a694888c2c186a08b7accda0a2a
Gal Attila <radeon16@hotmail.com>
2017-12-09 17:15:18 +0200

    - initial commit

---
+++ Utilities/StringHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Utilities
+{
+    public class StringHelpers
+    {
+        public static string ToCamelCase(string inputSentence)
+        {
+            // If there are 0 or 1 characters, just return the string.
+            if (inputSentence == null || inputSentence.Length < 2)
+                return inputSentence;
+
+            // Split the string into words.
+            string[] words = inputSentence.Split(
+                new char[] { },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            // Combine the words.
+            string camelCasedSentence = words[0].ToLower();
+            for (int i = 1; i < words.Length; i++)
+            {
+                camelCasedSentence +=
+                    words[i].Substring(0, 1).ToUpper() +
+                    words[i].Substring(1);
+            }
+
+            return camelCasedSentence;
+        }
+    }
+}
