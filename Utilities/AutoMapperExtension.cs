80dbc2b2291aade29a8f52f601037d83c8c1a24e
Gal Attila <radeon16@hotmail.com>
2017-12-09 19:33:56 +0200

    - updated nuget packages

---
+++ Utilities/AutoMapperExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using AutoMapper;
+
+namespace Utilities
+{
+    public static class AutoMapperExtension
+    {
+        public static IMappingExpression<TSource, TDestination> Ignore<TSource, TDestination>(
+            this IMappingExpression<TSource, TDestination> map,
+            Expression<Func<TDestination, object>> selector)
+        {
+            map.ForMember(selector, config => config.Ignore());
+            return map;
+        }
+
+        public static IMappingExpression<TSource, TDestination> IgnoreAllNonExisting<TSource, TDestination>(this IMappingExpression<TSource, TDestination> expression)
+        {
+            var sourceType = typeof(TSource);
+            var destinationType = typeof(TDestination);
+            var existingMaps = Mapper.Configuration.GetAllTypeMaps().First(x => x.SourceType == sourceType
+                                                                                && x.DestinationType == destinationType);
+            foreach (var property in existingMaps.GetUnmappedPropertyNames())
+            {
+                expression.ForMember(property, opt => opt.Ignore());
+            }
+            return expression;
+        }
+    }
+}
